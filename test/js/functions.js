d9330e1fd9149af5a25bad10e99aa987f37f48b2
EduardoLAV <eduardo.lopezarce@gmail.com>
2014-02-09 15:23:17 -0600

    style with js, img for loading call only once

--- test/js/functions.js
+++ test/js/functions.js
@@ -1,14 +1,16 @@
 /********JQuery*********/
 /*************Display div when images are not loaded*******************/
 document.getElementById("hideAll").style.display = "block";
+var windowHeight = $(window).height();
+var imgHeight = $("#imgLoad").height();
+$("#hideAll").css("padding-top",Math.round(windowHeight/2)-Math.round(imgHeight/2));
+
 $(document).ready(style);
 $(window).ready(style);
 $(window).resize(style);
 
-function style(){
-	var windowHeight = $(window).height();
-	var imgHeight = $("#imgLoad").height();
-	$("#hideAll").css("padding-top",Math.round(windowHeight/2)-Math.round(imgHeight/2));
+function style(){	
+	/*Style for containers*/
 	var mainContainerHeight1 = $("#mainContainer1").height();
 	var mainTextHeight1 = $("#mainText1").height();
 	$('#mainText1').css("margin-top",Math.round(mainContainerHeight1/2-mainTextHeight1/2));
