e2ca98d50177abc20b8da7c41d0a6f40f816cb57
EduardoLAV <eduardo.lopezarce@gmail.com>
2014-02-09 15:35:25 -0600

    style with js

--- test/js/functions.js
+++ test/js/functions.js
@@ -5,12 +5,10 @@ var windowHeight = $(window).height();
 var imgHeight = $("#imgLoad").height();
 $("#hideAll").css("padding-top",Math.round(windowHeight/2)-Math.round(imgHeight/2));
 
-$(document).ready(style);
-$(window).ready(style);
-$(window).resize(style);
 
 function style(){	
 	/*Style for containers*/
+	//alert("hola");
 	var mainContainerHeight1 = $("#mainContainer1").height();
 	var mainTextHeight1 = $("#mainText1").height();
 	$('#mainText1').css("margin-top",Math.round(mainContainerHeight1/2-mainTextHeight1/2));
@@ -18,4 +16,8 @@ function style(){
 
 $(window).load(function(){  
 	document.getElementById("hideAll").style.display = "none";
+	//alert("adios");
+	$(document).ready(style);
+	$(window).ready(style);
+	$(window).resize(style);
 });
