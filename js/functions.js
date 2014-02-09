a3b858ad36ebb3858bd219732d58b6818d771f26
EduardoLAV <eduardo.lopezarce@gmail.com>
2014-02-09 00:05:42 -0600

    load gray and logo

--- js/functions.js
+++ js/functions.js
@@ -1,6 +1,9 @@
-
 document.getElementById("hideAll").style.display = "block";
-			
+var windowHeight = $(window).height();
+var imgHeight = $("#imgLoad").height();
+$("#hideAll").css("padding-top",Math.round(windowHeight/2)-Math.round(imgHeight/2));
+//alert(imgHeight);
+
 $(window).load(function(){  
 	document.getElementById("hideAll").style.display = "none"; 
 });
