fa4a829893fcca442e121f5fc2938402aed89fed
EduardoLAV <eduardo.lopezarce@gmail.com>
2014-02-09 01:01:20 -0600

    footer align left

--- js/functions.js
+++ js/functions.js
@@ -5,7 +5,6 @@ var windowHeight = $(window).height();
 var imgHeight = $("#imgLoad").height();
 $("#hideAll").css("padding-top",Math.round(windowHeight/2)-Math.round(imgHeight/2));
 //alert(imgHeight);
-$("ul#navmenu ul.sub1:after").css("left","100%");
 
 $(window).load(function(){  
 	document.getElementById("hideAll").style.display = "none"; 
